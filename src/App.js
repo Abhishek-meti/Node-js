ea7c7c66f8f1b6a9d8a984c524b51fb16b609566
Abhishek-meti <135748714+Abhishek-meti@users.noreply.github.com>
2023-06-07 22:46:13 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,44 +1,146 @@
-import React, { useState } from 'react';
-import ContactForm from './ContactForm';
-import ContactList from './ContactList';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Line } from 'react-chartjs-2';
+import 'leaflet/dist/leaflet.css';
 
 const App = () => {
-  const [contacts, setContacts] = useState([]);
-  const [currentContact, setCurrentContact] = useState(null);
+  const [worldData, setWorldData] = useState(null);
+  const [countryData, setCountryData] = useState([]);
+  const [graphData, setGraphData] = useState(null);
 
-  const addContact = (contact) => {
-    const newContact = { ...contact, id: Date.now() };
-    setContacts([...contacts, newContact]);
+  useEffect(() => {
+    fetchData();
+    fetchGraphData();
+    fetchCountryData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://disease.sh/v3/covid-19/all');
+      setWorldData(response.data);
+    } catch (error) {
+      console.error('Error fetching worldwide data:', error);
+    }
   };
 
-  const viewContactDetails = (contactId) => {
-    const contact = contacts.find((c) => c.id === contactId);
-    setCurrentContact(contact);
+  const fetchGraphData = async () => {
+    try {
+      const response = await axios.get(
+        'https://disease.sh/v3/covid-19/historical/all?lastdays=all'
+      );
+      setGraphData(response.data);
+    } catch (error) {
+      console.error('Error fetching graph data:', error);
+    }
   };
 
-  const deleteContact = (contactId) => {
-    const updatedContacts = contacts.filter((c) => c.id !== contactId);
-    setContacts(updatedContacts);
-    setCurrentContact(null);
+  const fetchCountryData = async () => {
+    try {
+      const response = await axios.get(
+        'https://disease.sh/v3/covid-19/countries'
+      );
+      setCountryData(response.data);
+    } catch (error) {
+      console.error('Error fetching country data:', error);
+    }
+  };
+
+  const renderGraph = () => {
+    if (!graphData) {
+      return null;
+    }
+
+    const casesData = graphData.cases;
+    const dates = Object.keys(casesData);
+    const cases = Object.values(casesData);
+
+    const chartData = {
+      labels: dates,
+      datasets: [
+        {
+          label: 'Cases',
+          data: cases,
+          fill: false,
+          borderColor: '#007BFF',
+        },
+      ],
+    };
+
+    const chartOptions = {
+      scales: {
+        x: {
+          type: 'time',
+          time: {
+            unit: 'day',
+            displayFormats: {
+              day: 'MMM D',
+            },
+            tooltipFormat: 'll',
+          },
+          title: {
+            display: true,
+            text: 'Date',
+          },
+        },
+        y: {
+          beginAtZero: true,
+          title: {
+            display: true,
+            text: 'Cases',
+          },
+        },
+      },
+    };
+
+    return <Line data={chartData} options={chartOptions} />;
+  };
+
+  const renderMap = () => {
+    if (!countryData.length) {
+      return null;
+    }
+
+    return (
+      <MapContainer style={{ height: '400px', width: '100%' }} center={[0, 0]} zoom={2}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors"
+        />
+        {countryData.map((country) => (
+          <Marker key={country.country} position={[country.countryInfo.lat, country.countryInfo.long]}>
+            <Popup>
+              <div>
+                <h4>{country.country}</h4>
+                <p>Total Active Cases: {country.active}</p>
+                <p>Total Recovered Cases: {country.recovered}</p>
+                <p>Total Deaths: {country.deaths}</p>
+              </div>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    );
   };
 
   return (
     <div>
-      <h1>Contact Management App</h1>
-      <ContactForm addContact={addContact} />
-      <ContactList
-        contacts={contacts}
-        viewContactDetails={viewContactDetails}
-        deleteContact={deleteContact}
-      />
-      {currentContact && (
+      <h1>COVID-19 Dashboard</h1>
+      <h2>Worldwide Cases</h2>
+      {worldData && (
         <div>
-          <h2>Contact Details</h2>
-          <p>First Name: {currentContact.firstName}</p>
-          <p>Last Name: {currentContact.lastName}</p>
-          <button onClick={() => setCurrentContact(null)}>Close Details</button>
+          <p>Total Cases: {worldData.cases}</p>
+          <p>Total Deaths: {worldData.deaths}</p>
+          <p>Total Recovered: {worldData.recovered}</p>
+          <p>Total Active Cases: {worldData.active}</p>
         </div>
       )}
+
+      
+      <h2>Graph: Cases Over Time</h2>
+      {renderGraph()}
+      <h2>Country-wise Cases</h2>
+      {renderMap()}
     </div>
   );
 };
